d2645ca9596abb632c78824b0f5b886b64ba79e1
Anton <anton_real_madrid@icloud.com>
2020-03-12 03:11:25 +0100

    Started working on event system

---
+++ antech-engine/KeyEvent.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace Antech {
+	class ANTECH_API KeyEvent : public Event {
+	public:
+		inline int GetKeyCode() const { return m_KeyCode; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+	protected:
+		KeyEvent(int keycode)
+			: m_KeyCode(keycode) {}
+
+		int m_KeyCode;
+	};
+
+	class ANTECH_API KeyPressedEvent : public KeyEvent {
+	public:
+		KeyPressedEvent(int keycode, int repeatCount)
+			: KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+	};
+}
