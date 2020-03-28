a9617b9c976052a35b59bcc9c51f0938ee1b370c
Anton <anton_real_madrid@icloud.com>
2020-03-28 16:21:46 +0100

    Added LayerStack to Application

--- antech-engine/src/antech/Application.h
+++ antech-engine/src/antech/Application.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include "Core.h";
-#include "Events/Event.h"
-#include "antech/Events/ApplicationEvent.h"
 
 #include "Window.h"
+#include "antech/LayerStack.h"
+#include "antech/Events/Event.h"
+#include "antech/Events/ApplicationEvent.h"
+
 
 namespace Antech {
 
@@ -17,12 +19,16 @@ namespace Antech {
 		void Run();
 
 		void OnEvent(Event& e);
+
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* overlay);
 		
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
+		LayerStack m_LayerStack;
 	};
 
 	// To be defined in CLIENT
